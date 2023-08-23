8ca9e9e853a61a2cda8adc9472867cf21d6a732f
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-08-23 19:42:37 +0000

    not finished yet

---
+++ javascript/hashTable/index.js
@@ -0,0 +1,11 @@
+class Hashmap{
+    constructor(size){
+        this.size= size
+        this.map= new Array(size)
+    }
+    set(key,value){}
+    get(key){}
+    has(key){}
+    keys(){}
+    hash(key){}
+}
