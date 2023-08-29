262f77697186322ab18a319f8382a60649b6c956
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-08-29 19:04:08 +0000

    edited

--- javascript/hashTable/codeChallenge31/__test___/index.test.js
+++ javascript/hashTable/codeChallenge31/__test___/index.test.js
@@ -1,24 +1,25 @@
-const Hashmap = require('../index'); 
+const repeatedWord = require('../index'); 
 
 describe('Hashmap', () => {
-    let hashmap;
+    // let hashmap;
 
     beforeEach(() => {  // either put it like this or define it in each it test , like the commented ones
-        hashmap = new Hashmap(10);
+        // hashmap = new Hashmap(10);
+        
     });
 
     describe('repeatedWord', () => {
         it('should return the first repeated word in the string', () => {
         //    let  hashmap = new Hashmap(10);
             const inputString = "Once upon a time, there was a brave princess who...";
-            const result = hashmap.repeatedWord(inputString);
+            const result = repeatedWord(inputString);
             expect(result).toEqual('a');
         });
 
         it('should return null if no repeated word is found', () => {
             // let  hashmap = new Hashmap(10);
             const inputString = "This is a test string with no repeated words.";
-            const result = hashmap.repeatedWord(inputString);
+            const result =repeatedWord(inputString);
             expect(result).toBeNull();
         });
     });
